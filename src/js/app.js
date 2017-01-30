6fb8af8c23e616fe66c371b3e9a75d74cc392791
raginidua <raginidua1@gmail.com>
2017-01-30 13:57:40 +0000

    Module set up in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
 // app.js is the main JS file which you should define your Angular module
 console.log('JS loaded');
 
-(function($){
-  $(function(){
+angular
+  .module('teamBuilder', [
 
-    $('.button-collapse').sideNav();
+  ]);
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+// 
+// (function($){
+//   $(function(){
+//
+//     $('.button-collapse').sideNav();
+//
+//   }); // end of document ready
+// })(jQuery); // end of jQuery name space
