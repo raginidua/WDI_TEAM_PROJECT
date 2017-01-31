283eb604121ba8a011820d2e45f24b35e06741bb
raginidua <raginidua1@gmail.com>
2017-01-31 19:00:19 +0000

    Worked on the apply logic

--- src/js/controllers/projects-show.controller.js
+++ src/js/controllers/projects-show.controller.js
@@ -2,21 +2,30 @@ angular
   .module('teamBuilder')
   .controller('ProjectsShowCtrl', ProjectsShowCtrl);
 
-ProjectsShowCtrl.$inject = ['$stateParams', '$http'];
-function ProjectsShowCtrl($stateParams, $http) {
+ProjectsShowCtrl.$inject = ['$stateParams', '$http', '$state'];
+function ProjectsShowCtrl($stateParams, $http, $state) {
   const vm = this;
-  // vm.reqTeam = {
-  //   ux: 3,
-  //   ui: 2
-  // };
-  // vm.openTeam = {
-  //   ux: 3,
-  //   ui: 2
-  // };
   $http
     .get(`http://localhost:3000/api/projects/${$stateParams.id}`)
     .then(response => {
       vm.project = response.data.project;
     });
 
+  vm.freelancerApply = function(role, projectId) {
+    const currentUserId = '5890dc47bcc7a1201b09a904';
+    $http
+      .get(`http://localhost:3000/api/freelancers/${currentUserId}`)
+      .then(response => {
+        console.log('GET THE FREELANCER: ', response.data.freelancer);
+        vm.freelancer = response.data.freelancer;
+        vm.freelancer.pendingProjects.push(projectId);
+        console.log('UPDATE THE FREELANCER:', response.data.freelancer);
+        console.log('PROJECT BEFORE THE SAVE', vm.project);
+        vm.project.waitingTeamMembers[role].push(currentUserId);
+        console.log('PROJECT AFTER THE SAVE', vm.project);
+      });
+    $http
+      .put(`http://localhost:3000/api/freelancers/${currentUserId}`, vm.freelancer)
+      .put(`http://localhost:3000/api/projects/${projectId}`, vm.project);
+  };
 }
