b307e274ea901141112beed6dc67dc657b175bd4
jackhkmatthews <jackhkmatthews@gmail.com>
2017-01-31 12:22:28 +0000

    project create

--- src/js/controllers/projects-new.controller.js
+++ src/js/controllers/projects-new.controller.js
@@ -10,15 +10,32 @@ function ProjectsNewCtrl($http, $state) {
   vm.part1 = true;
 
   vm.createProject = function() {
-    //hard coded in freelancer ID pre authetication work
-    vm.newProject.project.leadFreelancer = '588e507a4d0d1f74979be1ee';
-    return $http
-      .post('http://localhost:3000/api/projects', vm.newProject)
-      .then(response => {
-        console.log(response);
-        //redirect to project show page of newly created project
-        $state.go('projectsShow', {id: response.data.project._id});
-      });
+    const object = {};
+    for (var role in vm.newProject.project.teamMembers) {
+      console.log(role);
+      console.log(vm.newProject.project.teamMembers[role]);
+      const array = [];
+      const numberRequired = vm.newProject.project.teamMembers[role];
+      for (var i = 0; i < numberRequired; i++) {
+        array.push(true);
+      }
+      object[role] = array;
+    }
+    vm.newProject.requiredTeamMembers = object;
+    // vm.newProject.requiredTeamMembers = object;
+    // vm.newProject.requiredTeamMembers = object;
+    // vm.newProject.requiredTeamMembers = object;
+    console.log(vm.newProject);
+
+    // //hard coded in freelancer ID pre authetication work
+    // vm.newProject.project.leadFreelancer = '588e507a4d0d1f74979be1ee';
+    // return $http
+    //   .post('http://localhost:3000/api/projects', vm.newProject)
+    //   .then(response => {
+    //     console.log(response);
+    //     //redirect to project show page of newly created project
+    //     $state.go('projectsShow', {id: response.data.project._id});
+    //   });
   };
 
 }
