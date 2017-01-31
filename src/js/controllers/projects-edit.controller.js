e7cf760d6d49bb908153f9573f348f188719ee12
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-01-31 18:04:44 +0000

    user can be moved from pending to live arrays

--- src/js/controllers/projects-edit.controller.js
+++ src/js/controllers/projects-edit.controller.js
@@ -19,16 +19,11 @@ function ProjectsEditCtrl($http, $state, $stateParams) {
   vm.acceptApplication =  acceptApplication;
 
   function acceptApplication(role, memberid) {
-    // console.log('ROLE', role);
-    // console.log('MEMBERID', memberid);
 
-    const applicantsArray = vm.project.waitingTeamMembers[role];
-    // console.log(applicantsArray);
-    const index = applicantsArray.indexOf(memberid);
-    // console.log(index);
-    //the push and pull from the arrays needs to happen here
+    const applicantsIndex = vm.project.waitingTeamMembers[role].indexOf(memberid);
 
-
+    vm.project.waitingTeamMembers[role].splice(applicantsIndex, 1);
+    vm.project.liveTeamMembers[role].push(memberid);
 
     // return $http
     // .put(`http://localhost:3000/api/projects/${vm.project._id}`, vm.project)
