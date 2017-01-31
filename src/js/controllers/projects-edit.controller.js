f24a6b0de6acf92da4514078ca612c7ae21e1447
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-01-31 18:32:34 +0000

    all done i think

--- src/js/controllers/projects-edit.controller.js
+++ src/js/controllers/projects-edit.controller.js
@@ -19,17 +19,23 @@ function ProjectsEditCtrl($http, $state, $stateParams) {
   vm.acceptApplication =  acceptApplication;
 
   function acceptApplication(role, memberid) {
-
+    //remove applicant from waiting applicants list
     const applicantsIndex = vm.project.waitingTeamMembers[role].indexOf(memberid);
-
     vm.project.waitingTeamMembers[role].splice(applicantsIndex, 1);
+
+    //add applicant to live team
     vm.project.liveTeamMembers[role].push(memberid);
 
-    // return $http
-    // .put(`http://localhost:3000/api/projects/${vm.project._id}`, vm.project)
-    // .then(() => {
-    //   $state.go('usersIndex');
-    // });
+    //open positions needs to go down
+    var availablePositions = vm.project.openTeamMembers[role];
+    availablePositions--;
+    vm.project.openTeamMembers[role] = availablePositions;
+    
+    return $http
+    .put(`http://localhost:3000/api/projects/${vm.project._id}`, vm.project)
+    .then(() => {
+      $state.go('projectsShow', {id: vm.project._id});
+    });
   }
 
 }
