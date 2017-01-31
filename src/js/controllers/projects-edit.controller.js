915c8359285724fb8f7ddfb911827de95d8cfa33
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-01-31 10:34:23 +0000

    the controller has basic show and put methods however more logic is needed before the put

--- src/js/controllers/projects-edit.controller.js
+++ src/js/controllers/projects-edit.controller.js
@@ -1,9 +1,29 @@
 angular
-  .module('teamBuilder')
-  .controller('ProjectsEditCtrl', ProjectsEditCtrl);
+.module('teamBuilder')
+.controller('ProjectsEditCtrl', ProjectsEditCtrl);
 
-ProjectsEditCtrl.$inject = [];
-function ProjectsEditCtrl() {
+ProjectsEditCtrl.$inject = ['$http', '$state', '$stateParams'];
+function ProjectsEditCtrl($http, $state, $stateParams) {
   const vm = this;
+
+  projectsShow();
+
+  function projectsShow(){
+    return $http
+    .get(`http://localhost:3000/api/projects/${$stateParams.id}`)
+    .then(response => {
+      vm.project = response.data;
+    });
+  }
+
+  vm.update = function projectsUpdate(){
+    //the push and pull from the arrays needs to happen here
+
+    return $http
+    .put(`http://localhost:3000/api/projects/${vm.project._id}`, vm.project)
+    .then(() => {
+      $state.go('usersIndex');
+    });
+  };
 
 }
