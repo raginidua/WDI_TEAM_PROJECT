c8d0f94f573fb080b109e696eeb5f8060ff442fe
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-02 20:01:10 +0000

    fl show controller update

--- src/js/controllers/freelancers/freelancer-show.controller.js
+++ src/js/controllers/freelancers/freelancer-show.controller.js
@@ -15,10 +15,7 @@ function FreelancersShowCtrl($stateParams, Freelancer, $scope, CurrentFreelancer
   .then(response => {
     vm.freelancer = response.freelancer;
   });
-
-  //sets profile id so can be compared to currentFreelancer ID
-  // vm.profileId = $stateParams.id;
-
+  
   //intialises (runs) materialize collapsible collection/accordion
   $('.collapsible').collapsible();
 
