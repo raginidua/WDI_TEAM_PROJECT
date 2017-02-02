c7aa080df46000a324fb0405380627147ad9276c
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-02 12:44:10 +0000

    user can select a value from the role dropdown to search

--- src/js/controllers/roles/roles-index.controller.js
+++ src/js/controllers/roles/roles-index.controller.js
@@ -2,9 +2,13 @@ angular
 .module('teamBuilder')
 .controller('RolesIndexCtrl', RolesIndexCtrl);
 
-RolesIndexCtrl.$inject = ['$http'];
-function RolesIndexCtrl($http) {
+RolesIndexCtrl.$inject = ['$http', 'rolesArray'];
+function RolesIndexCtrl($http, rolesArray) {
   const vm = this;
+
+  vm.rolesArray = rolesArray;
+  vm.searchCriteria;
+
 
   $http
   .get('http://localhost:3000/api/projects')
@@ -12,30 +16,25 @@ function RolesIndexCtrl($http) {
     vm.projects = response.data.projects;
   });
 
-  // vm.getTeamSizeNRoles = function(requiredTeamMembersObject) {
-  //   var teamSize = 0;
-  //   var teamRoles = '';
-  //   for(var role in requiredTeamMembersObject) {
-  //     if(requiredTeamMembersObject[role] > 0) {
-  //       teamSize+= requiredTeamMembersObject[role];
-  //       if(teamRoles === ''){
-  //         teamRoles+= role;
-  //       } else {
-  //         teamRoles+=', '+ role;
-  //       }
-  //     }
-  //   }
-  //   return teamSize + '[' + teamRoles + ']';
-  // };
-  //
-  // vm.getTeamSize = function(requiredTeamMembersObject) {
-  //   var teamSize = 0;
-  //   for(var role in requiredTeamMembersObject) {
-  //     if(requiredTeamMembersObject[role] > 0) {
-  //       teamSize+= requiredTeamMembersObject[role];
-  //     }
-  //   }
-  //   return teamSize;
-  // };
+  vm.searchForRole = function(){
+    console.log('SEARCHING FOR', vm.searchCriteria);
+  };
+
+  vm.getTeamSizeNRoles = function(requiredTeamMembersObject) {
+    var teamSize = 0;
+    var teamRoles = '';
+    for(var role in requiredTeamMembersObject) {
+      if(requiredTeamMembersObject[role] > 0) {
+        teamSize+= requiredTeamMembersObject[role];
+        if(teamRoles === ''){
+          teamRoles+= role;
+        } else {
+          teamRoles+=', '+ role;
+        }
+      }
+    }
+    return teamSize + '[' + teamRoles + ']';
+  };
+
 
 }
