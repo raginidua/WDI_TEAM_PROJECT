f2000065bae887fb095c7810b2f94367e71f74d4
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-02 13:58:59 +0000

    logic for role search exists and works

--- src/js/controllers/roles/roles-index.controller.js
+++ src/js/controllers/roles/roles-index.controller.js
@@ -8,27 +8,36 @@ function RolesIndexCtrl($http, rolesArray) {
 
   vm.rolesArray = rolesArray;
   vm.searchCriteria;
-
-  $http
-  .get('http://localhost:3000/api/projects')
-  .then(response => {
-    vm.projects = response.data.projects;
-  });
-
   vm.filteredProjects = [];
+
+  vm.getProjects = function() {
+    $http
+    .get('http://localhost:3000/api/projects')
+    .then(response => {
+      vm.projects = response.data.projects;
+    });
+  };
+
+  vm.getProjects();
 
   vm.searchForRole = function(){
     console.log('BUTTON HAS BEEN CLICKED TO SEARCH FOR', vm.searchCriteria);
 
-    for(var i = 0; i < vm.projects.length; i++){
-      for(var role in vm.projects[i].openTeamMembers) {
-        console.log('CHECKING', vm.projects[i].name, '__TEAMROLES__:', vm.projects[i].openTeamMembers);
-
-        if (role === vm.searchCriteria && vm.projects[i].openTeamMembers[role] > 0) {
-          console.log('MATCH and available');
-          vm.filteredProjects.push(vm.projects[i]);
+    $http
+    .get('http://localhost:3000/api/projects')
+    .then(response => {
+      vm.projects = response.data.projects;
+      for(var i = 0; i < vm.projects.length; i++){
+        for(var role in vm.projects[i].openTeamMembers) {
+          if (role === vm.searchCriteria && vm.projects[i].openTeamMembers[role] > 0) {
+            console.log('MATCH and available in ', vm.projects[i].name);
+            vm.filteredProjects.push(vm.projects[i]);
+          }
         }
       }
-    }
+      console.log('MATCHED PROJECTS', vm.filteredProjects);
+      vm.projects = vm.filteredProjects;
+      vm.filteredProjects = [];
+    });
   };
 }
