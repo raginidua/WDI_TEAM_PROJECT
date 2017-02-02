4b9591fb5f6cd7036c28771eb98df919f728136c
jackhkmatthews <jackhkmatthews@gmail.com>
2017-02-02 15:02:45 +0000

    commented factories

--- src/js/factories/auth-interceptor.factory.js
+++ src/js/factories/auth-interceptor.factory.js
@@ -2,6 +2,11 @@ angular
   .module('teamBuilder')
   .factory('AuthInterceptor', AuthInterceptor);
 
+
+//interceptor used to intercept requests and responses
+//on the way out attached token to head of requests
+//on the way in takes token from resopnse and sets to
+//local storage (part of the users browser memory)
 AuthInterceptor.$inject = ['API', 'TokenService'];
 function AuthInterceptor(API, TokenService) {
   return {
--- src/js/factories/freelancer.factory.js
+++ src/js/factories/freelancer.factory.js
@@ -2,6 +2,16 @@ angular
 .module('teamBuilder')
 .factory('Freelancer', freelancerFactory);
 
+
+//Shortcut for making requests to our freelancer database
+//defaults include:
+// { 'get':    {method:'GET'},
+//   'save':   {method:'POST'},
+//   'query':  {method:'GET', isArray:true},
+//   'remove': {method:'DELETE'},
+//   'delete': {method:'DELETE'} };
+
+
 freelancerFactory.$inject = ['API','$resource'];
 function freelancerFactory(API, $resource) {
   return $resource(`${API}/freelancers/:id`, { id: '@_id'}, {
