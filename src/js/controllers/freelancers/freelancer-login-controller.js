eca90ed48aca643f636b4ea1e7917674ce79d5c3
jackhkmatthews <jackhkmatthews@gmail.com>
2017-02-02 16:14:28 +0000

    commented freelancers controllers

--- src/js/controllers/freelancers/freelancer-login-controller.js
+++ src/js/controllers/freelancers/freelancer-login-controller.js
@@ -2,15 +2,22 @@ angular
   .module('teamBuilder')
   .controller('FreelancersLoginCtrl', FreelancersLoginCtrl);
 
-FreelancersLoginCtrl.$inject = ['$http','$state','Freelancer', 'TokenService', 'CurrentFreelancerService'];
-function FreelancersLoginCtrl($http, $state, Freelancer, TokenService, CurrentFreelancerService ){
+FreelancersLoginCtrl.$inject = ['Freelancer', 'CurrentFreelancerService'];
+function FreelancersLoginCtrl(Freelancer, CurrentFreelancerService ){
   const vm = this;
 
+  //uses our Freelancer factory (shorthand ajax) to send
+  //freelancer object (built by angualr in html) to our
+  //database and return user and token.
   vm.freelancersLogin = function freelancersLogin() {
     Freelancer
       .login(vm.freelancer.freelancer)
       .$promise
       .then(() =>{
+        //as token stored automattically by token service
+        //can use CurrentFreelancerService to decode token,
+        //make request to freelancer api and broadcast
+        //'logged in' to rest of app.
         CurrentFreelancerService.getFreelancer();
       }, err => {
         console.log(err);
