2ff3c853d9acf99cc587f83d2d0b4d7b8c6c2657
jackhkmatthews <jackhkmatthews@gmail.com>
2017-02-02 18:13:04 +0000

    html has been cleaned up

--- src/js/controllers/roles/roles-index.controller.js
+++ src/js/controllers/roles/roles-index.controller.js
@@ -2,8 +2,8 @@ angular
 .module('teamBuilder')
 .controller('RolesIndexCtrl', RolesIndexCtrl);
 
-RolesIndexCtrl.$inject = ['$http', 'rolesArray'];
-function RolesIndexCtrl($http, rolesArray) {
+RolesIndexCtrl.$inject = ['$http', 'rolesArray', 'Project'];
+function RolesIndexCtrl($http, rolesArray, Project) {
   const vm = this;
 
   vm.rolesArray = rolesArray;
@@ -21,17 +21,24 @@ function RolesIndexCtrl($http, rolesArray) {
   vm.getProjects();
 
   vm.searchForRole = function(){
-    $http
-    .get('http://localhost:3000/api/projects')
+    Project
+    .query()
+    .$promise
     .then(response => {
-      vm.projects = response.data.projects;
+      //get all the projects
+      vm.projects = response.projects;
+      //iterate through each project
       for(var i = 0; i < vm.projects.length; i++){
+        //run over the open members object
         for(var role in vm.projects[i].openTeamMembers) {
+          //check if the role is the same and there are open roles
           if (role === vm.searchCriteria && vm.projects[i].openTeamMembers[role] > 0) {
+            //if criteria is mateched theem push into a temp array
             vm.filteredProjects.push(vm.projects[i]);
           }
         }
       }
+      //replace the vm.projects array with the temp array
       vm.projects = vm.filteredProjects;
       vm.filteredProjects = [];
     });
